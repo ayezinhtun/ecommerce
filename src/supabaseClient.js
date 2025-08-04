77daa0bd44950e9dbb19b383586b262a45c7c752
ayezinhtun <ayezinhtun9@gmail.com>
2025-08-04 13:49:51 +0630

    update App.jsx

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://gxnmwpqnctlvndhudnho.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd4bm13cHFuY3Rsdm5kaHVkbmhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQwMzkwOTEsImV4cCI6MjA2OTYxNTA5MX0.EEVzq074Lj5nfpcB8kjqx30gm7a0KbHCO-O6kYhbK0o';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
